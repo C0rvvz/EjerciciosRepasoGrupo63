94336d4d209bb92c70f2403ecaea91423ea84708
Juan Diego <ajuandiego036@gmail.com>
2023-02-07 11:27:21 -0500

    Initial commit

--- ejercicios/ejercicio_1.py
+++ ejercicios/ejercicio_1.py
@@ -1,1 +1,8 @@
 if __name__ == "__main__":
+
+
+    nickname = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+
+
+print(f" Your nickname is {nickname} and your age is {age} years old ")
