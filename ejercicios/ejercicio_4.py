c997fcff0a3959d380d6789f9f496d592874db89
Juan Diego <ajuandiego036@gmail.com>
2023-02-09 18:56:18 -0500

    Ejercicio 4 hecho

---
+++ ejercicios/ejercicio_4.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+
+
+    numero = int(input("Cual es el numero?: "))
+
+
+    if numero % 2 == 0:
+
+
+        print("El numero es par")
+
+
+    else:
+        print("El numero no es par")
