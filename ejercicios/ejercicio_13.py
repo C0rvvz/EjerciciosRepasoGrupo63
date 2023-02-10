11a5e92403d0be20801b126810b2ddac4c6492b1
Juan Diego <ajuandiego036@gmail.com>
2023-02-10 01:01:47 -0500

    Ejercicio 13 hecho

---
+++ ejercicios/ejercicio_13.py
@@ -0,0 +1,17 @@
+if __name__ == "__main__":
+
+
+    numero_uno=int(input("ingrese el primer numero: "))
+    numero_dos=int(input("ingrese el segundo numero: "))
+
+    suma=numero_uno+numero_dos
+    print(f"la suma es: {suma}")
+
+    resta = numero_uno - numero_dos
+    print(f"la resta es: {resta}")
+
+    multiplicacion = numero_uno * numero_dos
+    print(f"la multiplicacion es: {multiplicacion}")
+
+    division = numero_uno / numero_dos
+    print(f"la division es: {division}")
